7e8b9f87856e8b4c43d3e2f47fc48fa58e281a29
Rashmi Jain <84361823+Rashmijain18@users.noreply.github.com>
2024-11-01 14:12:44 -0700

    sudoku implementation

---
+++ sudoku/src/SudoKu.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+
+const initialBoard = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9],
+];
+
+const Sudoku = () => {
+  const [board, setBoard] = useState(initialBoard);
+
+  const handleCellChange = (rowIndex, colIndex, value) => {
+    const val = parseInt(value);
+    if (!isValidNumber(val)) {
+      return;
+    }
+
+    if (!isValidNumberSelection(rowIndex, colIndex, val)) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[rowIndex][colIndex] = value === "" ? 0 : val;
+    setBoard(newBoard);
+  };
+
+  const isValidNumberSelection = (rowIndex, colIndex, inputValue) => {
+    //unique value in row
+    const rowValue = board[rowIndex];
+    if (rowValue.includes(inputValue)) {
+      return false;
+    }
+
+    //unique value in column
+    for (let i = 0; i <= board.length - 1; i++) {
+      if (board[i][colIndex] === inputValue) {
+        return false;
+      }
+    }
+
+    // Check 3x3 box
+    let boxRow = Math.floor(rowIndex / 3) * 3;
+    let boxCol = Math.floor(colIndex / 3) * 3;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[boxRow + i][boxCol + j] === inputValue) return false;
+      }
+    }
+
+    return true;
+  };
+
+  const isValidNumber = (value) => {
+    if (value > 0 && value < 10) {
+      return true;
+    }
+  };
+
+  const isDisabled = (rowIndex, colIndex) => {
+    if (initialBoard[rowIndex][colIndex] != 0) {
+      return true;
+    }
+    return false;
+  };
+
+  return (
+    <>
+      {board.map((row, rowIndex) => {
+        return (
+          <>
+            {row.map((col, colIndex) => {
+              return (
+                <>
+                  <input
+                    key={rowIndex + colIndex}
+                    value={col === 0 ? "" : col}
+                    onChange={(e) => {
+                      handleCellChange(rowIndex, colIndex, e.target.value);
+                    }}
+                    disabled={isDisabled(rowIndex, colIndex)}
+                  />
+                </>
+              );
+            })}
+          </>
+        );
+      })}
+    </>
+  );
+};
+
+export default Sudoku;
